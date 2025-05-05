28f0ad79d8e0b28a7ffe43dc24af792d8af34d6c
hoppla20 <privat@vincentcui.de>
2025-05-05 21:56:40 +0200

    feat: use eframe template to initialize gbemu_rust_app

---
+++ gbemu_rust_app/src/app.rs
@@ -0,0 +1,76 @@
+pub struct GbemuApp {
+    label: String,
+    counter: u32,
+}
+
+impl GbemuApp {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self {
+            label: "Hello World!".to_owned(),
+            counter: 0,
+        }
+    }
+}
+
+impl eframe::App for GbemuApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // the top panel is often a good place for a menu bar:
+
+            egui::menu::bar(ui, |ui| {
+                // no quit on web pages
+                if !cfg!(target_arch = "wasm32") {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Quit").clicked() {
+                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                        }
+                    });
+                    ui.add_space(16.0);
+                }
+
+                egui::widgets::global_theme_preference_buttons(ui);
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // the central panel the region left after adding TopPanel's and SidePanel's
+            ui.heading("eframe template");
+
+            ui.horizontal(|ui| {
+                ui.label("Write something: ");
+                ui.text_edit_singleline(&mut self.label);
+            });
+
+            ui.add(egui::Slider::new(&mut self.counter, 0..=10).text("value"));
+            if ui.button("Increment").clicked() {
+                self.counter += 1;
+            }
+
+            ui.separator();
+
+            ui.add(egui::github_link_file!(
+                "https://github.com/emilk/eframe_template/blob/main/",
+                "Source code."
+            ));
+
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                powered_by_egui_and_eframe(ui);
+                egui::warn_if_debug_build(ui);
+            });
+        });
+    }
+}
+
+fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
+    ui.horizontal(|ui| {
+        ui.spacing_mut().item_spacing.x = 0.0;
+        ui.label("Powered by ");
+        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+        ui.label(" and ");
+        ui.hyperlink_to(
+            "eframe",
+            "https://github.com/emilk/egui/tree/master/crates/eframe",
+        );
+        ui.label(".");
+    });
+}
--- gbemu_rust_app/src/main.rs
+++ gbemu_rust_app/src/main.rs
@@ -1,3 +1,77 @@
+// hide console window on Windows in release
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod app;
+
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    println!("Hello, world!");
+    use app::GbemuApp;
+    use tracing_subscriber::EnvFilter;
+
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_writer(std::io::stdout)
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    let native_options = eframe::NativeOptions {
+        // TODO: icons
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0]),
+        ..eframe::NativeOptions::default()
+    };
+
+    eframe::run_native(
+        "eframe template",
+        native_options,
+        Box::new(|cc| Ok(Box::new(GbemuApp::new(cc)))),
+    )
+    .unwrap();
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    use app::GbemuApp;
+    use eframe::wasm_bindgen::JsCast as _;
+
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("gbemu_rust_app_main")
+            .expect("Failed to find gbemu_rust_app_main")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("gbemu_rust_app_main was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|cc| Ok(Box::new(GbemuApp::new(cc)))),
+            )
+            .await;
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                },
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                },
+            }
+        }
+    });
 }
