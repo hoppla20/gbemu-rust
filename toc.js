99daf91fbd6af1b148a4028ebbb7c31c10c0497a
hoppla20 <privat@vincentcui.de>
2025-05-06 18:03:47 +0000

    Deploying to gh-pages from @ hoppla20/gbemu-rust@04074c13e2d19bbb0aebf21ac130c4825939af7a 🚀

--- toc.js
+++ toc.js
@@ -10,7 +10,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
     connectedCallback() {
         this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="001-introduction.html"><strong aria-hidden="true">1.</strong> Introduction</a></li><li class="chapter-item expanded "><a href="100-gameboy.html"><strong aria-hidden="true">2.</strong> Gameboy (empty)</a></li><li class="chapter-item expanded "><a href="200-code.html"><strong aria-hidden="true">3.</strong> Code (empty)</a></li><li class="chapter-item expanded "><a href="300-testing.html"><strong aria-hidden="true">4.</strong> Testing</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
-        let current_page = document.location.href.toString().split("#")[0];
+        let current_page = document.location.href.toString().split("#")[0].split("?")[0];
         if (current_page.endsWith("/")) {
             current_page += "index.html";
         }
